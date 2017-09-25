f318d2fb660211056d45f774ebd36da935a25d75
Haitham Ennasr <hthmnasr@gmail.com>
2017-09-25 15:25:08 -0400

    started implementing functionality in scene1

---
+++ Assets/_Scripts/ControllerInput.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControllerInput : MonoBehaviour {
+
+    public Transform rightControllerSphere;
+    public Transform leftControllerSphere;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(OVRInput.Get(OVRInput.Button.PrimaryHandTrigger) || OVRInput.Get(OVRInput.Button.PrimaryIndexTrigger))
+        {
+            Debug.Log("button pressed");
+            RaycastRightContoller();
+        }
+		
+	}
+
+    private void RaycastRightContoller()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(rightControllerSphere.position, rightControllerSphere.forward, out hit)){
+            if (hit.collider.gameObject.CompareTag("Sphere"))
+            {
+                Destroy(hit.collider.gameObject);
+            }
+        }
+    }
+}
