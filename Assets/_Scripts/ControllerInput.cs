7ab02a95c6fe33ad0aaeb82b00610b12d3b5a97c
Haitham Ennasr <hthmnasr@gmail.com>
2017-09-27 16:38:26 -0400

    added support for both touch contollers

--- Assets/_Scripts/ControllerInput.cs
+++ Assets/_Scripts/ControllerInput.cs
@@ -6,8 +6,9 @@ public class ControllerInput : MonoBehaviour {
 
     public LineRenderer rightLineRenderer;
 
-    public Transform rightController;
-    public Transform leftController;
+    public int sphereCounter;
+    public Transform rightControllerSphere;
+    public Transform leftControllerSphere;
 
 	// Use this for initialization
 	void Start () {
@@ -17,38 +18,103 @@ public class ControllerInput : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 
-  //      laserLineRenderer.SetPosition(0, rightController.transform.position);
-//        laserLineRenderer.SetPosition(1, rightController.transform.forward);
-
-        if (OVRInput.Get(OVRInput.Button.PrimaryHandTrigger) || OVRInput.Get(OVRInput.Button.PrimaryIndexTrigger))
+       // Check if right touch contoller is connected
+        if (OVRInput.IsControllerConnected(OVRInput.Controller.RTouch))
         {
-            rightLineRenderer.enabled = true;
+            rightControllerSphere.gameObject.SetActive(true);
 
-            RaycastRightContoller();
-            
+            if (OVRInput.Get(OVRInput.Button.PrimaryHandTrigger) || OVRInput.Get(OVRInput.Button.PrimaryHandTrigger))
+            {
+
+                rightLineRenderer.enabled = true;
+
+                RaycastRightContoller();
+
+            }
+            else
+            {
+                rightLineRenderer.enabled = false;
+            }
         }
-        else{
-            rightLineRenderer.enabled=false;
+
+        //if the rigt contoller is not connected disable the right contoller sphere
+        else
+        {
+            rightControllerSphere.gameObject.SetActive(false);
         }
-		
-	}
+
+       // check  if left contoller is connected
+
+        if (OVRInput.IsControllerConnected(OVRInput.Controller.LTouch))
+        {
+            leftControllerSphere.gameObject.SetActive(true);
+            if (OVRInput.Get(OVRInput.Button.PrimaryHandTrigger) || OVRInput.Get(OVRInput.Button.PrimaryIndexTrigger))
+            {
+
+                rightLineRenderer.enabled = true;
+
+                RaycastLeftContoller();
+
+            }
+            else
+            {
+                rightLineRenderer.enabled = false;
+            }
+        }
+
+        //if the left contoller is not connected disable the left contoller sphere
+        else
+        {
+            leftControllerSphere.gameObject.SetActive(false);
+        }
+    }
 
     private void RaycastRightContoller()
     {
+       
+        Ray ray = new Ray(rightControllerSphere.position, rightControllerSphere.forward);
         RaycastHit hit;
-        Ray ray = new Ray(rightController.position, rightController.forward);
 
         rightLineRenderer.SetPosition(0, ray.origin);
         rightLineRenderer.SetPosition(1, ray.GetPoint(500));
-       // Debug.DrawLine(ray.origin, ray.GetPoint(10));
+
 
 
         if (Physics.Raycast(ray, out hit)){
             if (hit.collider.gameObject.CompareTag("Sphere"))
             {
-                Destroy(hit.collider.gameObject);
+                destroySphere(hit.collider.gameObject);
                 
             }
         }
     }
+
+    private void RaycastLeftContoller()
+    {
+
+        Ray ray = new Ray(leftControllerSphere.position, leftControllerSphere.forward);
+        RaycastHit hit;
+
+        rightLineRenderer.SetPosition(0, ray.origin);
+        rightLineRenderer.SetPosition(1, ray.GetPoint(500));
+
+
+
+        if (Physics.Raycast(ray, out hit))
+        {
+            if (hit.collider.gameObject.CompareTag("Sphere"))
+            {
+                destroySphere(hit.collider.gameObject);
+
+            }
+        }
+    }
+
+    private void destroySphere(GameObject sphere)
+    {
+        Destroy(sphere);
+        sphereCounter--;
+        Debug.Log(sphereCounter);
+    }
+    
 }
